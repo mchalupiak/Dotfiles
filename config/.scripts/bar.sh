a6dfb0a474e8d63786839d92864e0f5c445ad9db
Michael Chalupiak <mikec@mchalupiak.com>
2024-09-05 20:01:50 -0400

    laptop settings

--- config/.scripts/bar.sh
+++ config/.scripts/bar.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 DATE=$(date +'%Y-%m-%d %I:%M:%S %p')
-# BAT=$(cat /sys/class/power_supply/BAT1/capacity)
+BAT=$(cat /sys/class/power_supply/BAT1/capacity)
 VOL=$(pactl get-sink-volume @DEFAULT_SINK@ | tr "," "\n" | grep \% | sed s/Volume\:// | sed 's/\%.*$//' | sed 's/^.*\///' | head -n1 | sed s/\ *//)
 VOL="${VOL}%"
 MUTED=$(pactl get-sink-mute @DEFAULT_SINK@ | cut -d\  -f2)
-# BRIGHT=$(brightnessctl | tr "\n" " " | cut -d\  -f9 | tr -d "()%")
+BRIGHT=$(brightnessctl | tr "\n" " " | cut -d\  -f9 | tr -d "()%")
 MEM=$(free -h | sed 's/[  ]\+/:/g' | cut -d\: -f4 | tr "\n" ":" | cut -d\: -f2)
 USER=$(id -u -n)
 HOST=$(cat /proc/sys/kernel/hostname)

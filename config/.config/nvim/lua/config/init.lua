3ebc1b414cb4ebebcac0679b8199b185d6fe5d9c
Michael Chalupiak <mikec@mchalupiak.com>
2025-08-17 11:14:52 -0400

    New neovim config

--- config/.config/nvim/lua/config/init.lua
+++ config/.config/nvim/lua/config/init.lua
@@ -1,3 +1,3 @@
-require("config.remap")
-require("config.packer")
-require("config.set")
+require 'config.lazy'
+require 'config.set'
+require 'config.keymap'
---
+++ config/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,110 @@
+return {
+
+    {
+        'mason-org/mason.nvim',
+        opts = {},
+    },
+
+    {
+        'neovim/nvim-lspconfig',
+    },
+
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+    },
+
+    {
+        'saghen/blink.cmp',
+        -- optional: provides snippets for the snippet source
+        dependencies = { 'L3MON4D3/LuaSnip' },
+
+        -- use a release tag to download pre-built binaries
+        version = '1.*',
+        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+        -- build = 'cargo build --release',
+        -- If you use nix, you can build from source using latest nightly rust with:
+        -- build = 'nix run .#build-plugin',
+
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+            -- 'super-tab' for mappings similar to vscode (tab to accept)
+            -- 'enter' for enter to accept
+            -- 'none' for no mappings
+            --
+            -- All presets have the following mappings:
+            -- C-space: Open menu or open docs if already open
+            -- C-n/C-p or Up/Down: Select next/previous item
+            -- C-e: Hide menu
+            -- C-k: Toggle signature help (if signature.enabled = true)
+            --
+            -- See :h blink-cmp-config-keymap for defining your own keymap
+            keymap = { preset = 'default' },
+
+            snippets = { preset = 'luasnip' },
+            appearance = {
+                -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                -- Adjusts spacing to ensure icons are aligned
+                nerd_font_variant = 'mono'
+            },
+
+            -- (Default) Only show the documentation popup when manually triggered
+            completion = {
+                documentation = { auto_show = false },
+                menu = {
+                    draw = {
+                        components = {
+                            -- customize the drawing of kind icons
+                            kind_icon = {
+                                text = function(ctx)
+                                    -- default kind icon
+                                    local icon = ctx.kind_icon
+                                    -- if LSP source, check for color derived from documentation
+                                    if ctx.item.source_name == "LSP" then
+                                        local color_item = require("nvim-highlight-colors").format(ctx.item.documentation, { kind = ctx.kind })
+                                        if color_item and color_item.abbr ~= "" then
+                                            icon = color_item.abbr
+                                        end
+                                    end
+                                    return icon .. ctx.icon_gap
+                                end,
+                                highlight = function(ctx)
+                                    -- default highlight group
+                                    local highlight = "BlinkCmpKind" .. ctx.kind
+                                    -- if LSP source, check for color derived from documentation
+                                    if ctx.item.source_name == "LSP" then
+                                        local color_item = require("nvim-highlight-colors").format(ctx.item.documentation, { kind = ctx.kind })
+                                        if color_item and color_item.abbr_hl_group then
+                                            highlight = color_item.abbr_hl_group
+                                        end
+                                    end
+                                    return highlight
+                                end,
+                            },
+                        },
+                    },
+                },
+            },
+
+            -- Default list of enabled providers defined so that you can extend it
+            -- elsewhere in your config, without redefining it, due to `opts_extend`
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+
+            -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+            --
+            -- See the fuzzy documentation for more information
+            fuzzy = { implementation = "prefer_rust_with_warning" }
+        },
+        opts_extend = { "sources.default" }
+    }
+}
